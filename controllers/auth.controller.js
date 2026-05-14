6bc920e856b7174f04cb88f0b567d658225b9e90
maradabhargavnaidu <bhargav.naidu.mbn.18@gmail.com>
2026-05-14 13:19:52 +0530

    update jwt secret

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const prisma = require("../config/prisma");
 
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET = process.env.JWT_SECRET || "secret";
 
 const register = async (req, res, next) => {
   try {
